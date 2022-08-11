1ef6181ff6776352e3f90b404d9115e0562e04f4
Ellie <Ellie.Ku@want-want.com>
2022-08-11 16:04:46 +0800

    fix : Heroku dynamically assigns your app a port, so you can't set the port to a fixed number

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const http = require('http');
+const PORT = process.env.PORT || 5000;
 
-// const hostname = '127.0.0.1';
-const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.end('hello'); // must be called on each response
 });
 
-server.listen(port, () => {
-  console.log(`Listening on ${ port }`)
+server.listen(PORT, () => {
+  console.log(`Listening on ${ PORT }`)
 });
